3117c5ab4e1c1d0920e9bc456fece0e24dd6749e
hosamshaban <97hosams@gmail.com>
2023-05-18 15:56:09 +0300

    create splash_view.dart file

---
+++ lib/features/splash/presentation/view/splash_view.dart
@@ -0,0 +1,13 @@
+import 'package:acthub/core/resources/manager_colors.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: ManagerColors.primaryColor,
+    );
+  }
+}
