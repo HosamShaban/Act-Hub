7c7e21088fe13916083bedc5a28dfbc19cc868e8
hosamshaban <97hosams@gmail.com>
2023-05-21 15:58:32 +0300

    Add appBarTheme , buttonTheme and textTheme in manager_light_theme.dart

--- lib/core/theme/manager_light_theme.dart
+++ lib/core/theme/manager_light_theme.dart
@@ -1,9 +1,29 @@
+import 'package:acthub/config/constants.dart';
 import 'package:acthub/core/resources/manager_colors.dart';
+import 'package:acthub/core/resources/manager_fonts.dart';
+import 'package:acthub/core/resources/manager_styles.dart';
+import 'package:acthub/core/resources/manager_text_theme_light.dart';
 import 'package:flutter/material.dart';
 
 ThemeData managerLightThemeData() {
   return ThemeData.light().copyWith(
-    useMaterial3: true,
-    primaryColor: ManagerColors.primaryColor,
-  );
+      useMaterial3: true,
+      primaryColor: ManagerColors.primaryColor,
+      primaryColorLight: ManagerColors.primaryColorLight,
+      disabledColor: ManagerColors.textColor,
+      splashColor: ManagerColors.greyLight,
+      scaffoldBackgroundColor: ManagerColors.scaffoldBackgroundColorLight,
+      appBarTheme: AppBarTheme(
+        centerTitle: true,
+        color: ManagerColors.greyLight,
+        elevation: Constants.elevationAppBar,
+        titleTextStyle: getBoldTextStyle(
+            fontSize: ManagerFontSize.s14, color: ManagerColors.primaryColor),
+      ),
+      buttonTheme: const ButtonThemeData(
+        shape: RoundedRectangleBorder(),
+        disabledColor: ManagerColors.greyLight,
+        buttonColor: ManagerColors.buttonColorLight,
+      ),
+      textTheme: ManagerTextThemeLight());
 }
