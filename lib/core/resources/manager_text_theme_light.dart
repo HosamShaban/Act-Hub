24fefb41a498431b7535f6f842119debb95825e3
hosamshaban <97hosams@gmail.com>
2023-05-21 15:59:05 +0300

    Add four function in manager_text_theme_light.dart

--- lib/core/resources/manager_text_theme_light.dart
+++ lib/core/resources/manager_text_theme_light.dart
@@ -1,3 +1,22 @@
+import 'package:acthub/core/resources/manager_colors.dart';
+import 'package:acthub/core/resources/manager_fonts.dart';
+import 'package:acthub/core/resources/manager_styles.dart';
 import 'package:flutter/material.dart';
 
-class ManagerTextThemeLight extends TextTheme {}
+class ManagerTextThemeLight extends TextTheme {
+  @override
+  TextStyle get displayMedium => getMediumTextStyle(
+      fontSize: ManagerFontSize.s20, color: ManagerColors.textColor);
+
+  @override
+  TextStyle get displaySmall => getMediumTextStyle(
+      fontSize: ManagerFontSize.s16, color: ManagerColors.textColor);
+
+  @override
+  TextStyle get headlineMedium => getMediumTextStyle(
+      fontSize: ManagerFontSize.s16, color: ManagerColors.textColor);
+
+  @override
+  TextStyle get titleMedium => getMediumTextStyle(
+      fontSize: ManagerFontSize.s14, color: ManagerColors.textColor);
+}
