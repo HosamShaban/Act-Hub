88596367d8187e09b76624015ffc14c17dfb428b
hosamshaban <97hosams@gmail.com>
2023-05-23 18:13:30 +0300

    add sizes to manager_fonts.dart

--- lib/core/resources/manager_fonts.dart
+++ lib/core/resources/manager_fonts.dart
@@ -10,6 +10,8 @@ class ManagerFontWight {
   static const FontWeight bold = FontWeight.w700;
   static const FontWeight regular = FontWeight.w400;
   static const FontWeight medium = FontWeight.w500;
+  static const FontWeight w200 = FontWeight.w200;
+  static const FontWeight w300 = FontWeight.w300;
 }
 
 class ManagerFontSize {
@@ -21,4 +23,5 @@ class ManagerFontSize {
   static double s14 = SizeUtil.setFontSize(14);
   static double s16 = SizeUtil.setFontSize(16);
   static double s20 = SizeUtil.setFontSize(20);
+  static double s34 = SizeUtil.setFontSize(34);
 }
