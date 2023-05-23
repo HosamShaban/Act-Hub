e93e00474ab237d928d65d74336670e7831f496d
hosamshaban <97hosams@gmail.com>
2023-05-23 18:13:08 +0300

    add new constants.dart

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,4 +4,7 @@ class Constants {
   static const double deviceHeight = 812;
   static const int splashDuration = 3;
   static const double elevationAppBar = 0;
+  static const double elevationButton = 0;
+  static const int outBoardingDurationTime = 1;
+  static const int sliderItems = 3;
 }
