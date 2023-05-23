03fe60856f2271da5586c86bf2e68a7df112fd2f
hosamshaban <97hosams@gmail.com>
2023-05-23 18:16:39 +0300

    create initOutBoarding in dependency_injection.dart

--- lib/config/dependency_injection.dart
+++ lib/config/dependency_injection.dart
@@ -1,3 +1,4 @@
+import 'package:acthub/features/out_boarding/presentation%20/controller/out_boarding_controller.dart';
 import 'package:acthub/features/splash/presentation/controller/splash_controller.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
@@ -8,10 +9,15 @@ initSplash() {
   Get.put<SplashController>(SplashController());
 }
 
-finishSplash() {
+disposeSplash() {
   Get.delete<SplashController>();
 }
 
 initOutBoarding() {
-  finishSplash();
+  disposeSplash();
+  Get.put<OutBoardingController>(OutBoardingController());
+}
+
+disposeOutBoarding() {
+  Get.delete<OutBoardingController>();
 }
