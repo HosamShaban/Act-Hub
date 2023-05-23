7154ce7b76565fd1fed201b8699f7e3636f840ff
hosamshaban <97hosams@gmail.com>
2023-05-23 18:11:58 +0300

    add assets

--- lib/core/resources/manager_assets.dart
+++ lib/core/resources/manager_assets.dart
@@ -2,8 +2,12 @@ const String imagesPath = 'assets/images';
 const String jsonPath = 'assets/json';
 
 class ManagerAssets {
-
   static const String background = '$imagesPath/background.png';
   static const String logo = '$imagesPath/logo.svg';
-
+  static const String outBoardingIllustration1 =
+      '$imagesPath/illustration1.svg';
+  static const String outBoardingIllustration2 =
+      '$imagesPath/illustration2.svg';
+  static const String outBoardingIllustration3 =
+      '$imagesPath/illustration3.svg';
 }
