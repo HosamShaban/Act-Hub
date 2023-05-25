3e563661dd20e23d1f0a5886c82c09ef613e55ea
hosamshaban <97hosams@gmail.com>
2023-05-25 14:04:54 +0300

    create two function setOutBoardingViewed and getOutBoardingViewed

--- lib/core/storage/local/app_settings_shared_preferences.dart
+++ lib/core/storage/local/app_settings_shared_preferences.dart
@@ -1,7 +1,20 @@
+import 'package:acthub/config/constants.dart';
+import 'package:acthub/core/extensions/extensions.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSettingsSharedPreferences {
   final SharedPreferences _sharedPreferences;
 
   AppSettingsSharedPreferences(this._sharedPreferences);
+
+  Future<void> setOutBoardingViewed() async {
+    await _sharedPreferences.setBool(
+        ConstantsPrefsKey.outBoardingViewedKey, true);
+  }
+
+  bool getOutBoardingViewed() {
+    return _sharedPreferences
+        .getBool(ConstantsPrefsKey.outBoardingViewedKey)
+        .onNull();
+  }
 }
