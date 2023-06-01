32715b6e0b38a6a5d75c9bc82999e039a51e5f19
hosamshaban <97hosams@gmail.com>
2023-06-01 15:42:15 +0300

    create Login View

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -3,52 +3,88 @@ import 'package:acthub/core/resources/manager_assets.dart';
 import 'package:acthub/core/resources/manager_colors.dart';
 import 'package:acthub/core/resources/manager_fonts.dart';
 import 'package:acthub/core/resources/manager_size.dart';
+import 'package:acthub/core/resources/manager_string.dart';
 import 'package:acthub/core/resources/manager_styles.dart';
+import 'package:acthub/core/validator/validator.dart';
+import 'package:acthub/core/widgets/base_text_form_field.dart';
+import 'package:acthub/core/widgets/scaffold_with_background_image.dart';
+import 'package:acthub/features/auth/presentation/controller/login_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 class LoginView extends StatelessWidget {
-  const LoginView({Key? key}) : super(key: key);
+  final FailedValidator _failedValidator = FailedValidator();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: ManagerColors.primaryColor,
-        body: Column(
-          children: [
-            Expanded(
-              flex: Constants.loginFirstPartFlex,
-              child: Container(
-                margin: EdgeInsetsDirectional.only(top: ManagerHeight.h12),
-                alignment: Alignment.center,
-                child: SvgPicture.asset(ManagerAssets.logo),
+    return scaffoldWithBackGroundImage(
+        child: Column(
+      children: [
+        Expanded(
+          flex: Constants.loginFirstPartFlex,
+          child: Container(
+            margin: EdgeInsetsDirectional.only(top: ManagerHeight.h12),
+            alignment: Alignment.center,
+            child: SvgPicture.asset(ManagerAssets.logo),
+          ),
+        ),
+        Expanded(
+          flex: Constants.loginSecondPartFlex,
+          child: Container(
+            decoration: BoxDecoration(
+              color: ManagerColors.backgroundForm,
+              borderRadius: BorderRadiusDirectional.only(
+                topStart: Radius.circular(
+                  ManagerRadius.r44,
+                ),
               ),
             ),
-            Expanded(
-              flex: Constants.loginSecondPartFlex,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: ManagerColors.white,
-                  borderRadius: BorderRadiusDirectional.only(
-                    topStart: Radius.circular(ManagerRadius.r44),
-                  ),
-                ),
-                padding: EdgeInsets.symmetric(
-                    horizontal: ManagerWidth.w16, vertical: ManagerHeight.h30),
-                width: double.infinity,
-                child: Column(
-                  children: [
-                    Text(
-                      'Login',
-                      style: getMediumTextStyle(
+            padding: EdgeInsets.symmetric(
+              horizontal: ManagerWidth.w16,
+              vertical: ManagerHeight.h30,
+            ),
+            width: double.infinity,
+            child: GetBuilder<LoginController>(
+              builder: (controller) {
+                return Form(
+                  key: controller.formKey,
+                  child: Column(
+                    children: [
+                      Text(
+                        ManagerString.login,
+                        style: getMediumTextStyle(
                           fontSize: ManagerFontSize.s24,
-                          color: ManagerColors.black),
-                    ),
-                  ],
-                ),
-              ),
+                          color: ManagerColors.black,
+                        ),
+                      ),
+                      SizedBox(height: ManagerHeight.h30),
+                      baseTextFormField(
+                        controller: controller.email,
+                        hintText: ManagerString.email,
+                        keyboardType: TextInputType.emailAddress,
+                        validator: (value) => _failedValidator.validateEmail(
+                          value,
+                        ),
+                      ),
+                      SizedBox(height: ManagerHeight.h16),
+                      baseTextFormField(
+                        controller: controller.password,
+                        hintText: ManagerString.password,
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        validator: (value) => _failedValidator.validatePassword(
+                          value,
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              },
             ),
-          ],
-        ));
+          ),
+        ),
+      ],
+    ));
   }
 }
