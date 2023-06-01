ffc171a85ab4f19a096f83a251ab71d17485a7b7
hosamshaban <97hosams@gmail.com>
2023-06-01 15:41:54 +0300

    create Login Controller

---
+++ lib/features/auth/presentation/controller/login_controller.dart
@@ -0,0 +1,22 @@
+import 'package:acthub/config/dependency_injection.dart';
+import 'package:acthub/features/auth/domain/use_case/login_use_case.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class LoginController extends GetxController {
+  late TextEditingController email = TextEditingController();
+  late TextEditingController password = TextEditingController();
+  late final LoginUseCase _loginUseCase = instance<LoginUseCase>();
+  var formKey = GlobalKey<FormState>();
+
+  Future<void> login() async {
+    (await _loginUseCase.execute(
+      LoginBaseUseCaseInput(email: email.text, password: password.text),
+    ))
+        .fold((l) {
+      print('failed');
+    }, (r) {
+      print('success');
+    });
+  }
+}
