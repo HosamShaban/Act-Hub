acbd4130441bbb80ba8f7f5e663977ac091ab3a2
hosamshaban <97hosams@gmail.com>
2023-06-04 16:47:08 +0300

    create home view

---
+++ lib/features/home/presentation/view/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:acthub/core/widgets/scaffold_with_background_image.dart';
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return scaffoldWithBackGroundImage(
+      child: Container(),
+    );
+  }
+}
