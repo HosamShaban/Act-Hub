e93fa340df4209c471307b6b392ea9415bfa1022
hosamshaban <97hosams@gmail.com>
2023-06-04 16:46:52 +0300

    add constants

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -8,11 +8,16 @@ class Constants {
   static const int sliderItems = 3;
   static const int loginFirstPartFlex = 1;
   static const int loginSecondPartFlex = 3;
+  static const double getStateWidgetRenderStateElevation = 3;
+  static const int loginTimer = 1;
 }
 
 class ConstantsPrefsKey {
   static const String outBoardingViewedKey = 'out_boarding_viewed';
-  static const String tokenKey = 'token';
+  static const String token = 'token';
+  static const String email = 'email';
+  static const String password = 'password';
+  static const String loggedIn = 'logged_in';
 }
 
 class ApiConstants {
@@ -28,4 +33,7 @@ class ApiConstants {
   static const String errors = 'errors';
   static const String badRequest = 'bad request';
   static const String noInternetConnection = ' No InternetConnection';
+  static const String acceptLanguage = 'Accept-Language';
+  static const String english = 'en';
+  static const String arabic = 'ar';
 }
