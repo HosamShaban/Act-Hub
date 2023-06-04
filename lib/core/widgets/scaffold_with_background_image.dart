53cbc533fe7d2a8a1bee7ff460d46ffba6826961
hosamshaban <97hosams@gmail.com>
2023-06-04 16:49:43 +0300

    edit scaffoldWithBackGroundImage

--- lib/core/widgets/scaffold_with_background_image.dart
+++ lib/core/widgets/scaffold_with_background_image.dart
@@ -1,4 +1,8 @@
+import 'package:acthub/config/constants.dart';
+import 'package:acthub/core/widgets/will_pop_scope.dart';
+import 'package:acthub/routes/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../resources/manager_assets.dart';
 import '../resources/manager_colors.dart';
@@ -7,20 +11,39 @@ Widget scaffoldWithBackGroundImage({
   required Widget child,
   Color backgroundColor = ManagerColors.primaryColor,
   String image = ManagerAssets.background,
+  bool isRegisterView = false,
 }) {
-  return Scaffold(
-    resizeToAvoidBottomInset: false,
-    backgroundColor: backgroundColor,
-    body: Container(
-      decoration: BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            image,
-          ),
-          fit: BoxFit.cover,
-        ),
+  return willPopScope(
+    child: Scaffold(
+      backgroundColor: backgroundColor,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: ManagerColors.transparent,
+        elevation: Constants.elevationAppBar,
+        leading: isRegisterView
+            ? IconButton(
+                onPressed: () {
+                  Get.offAllNamed(Routes.loginView);
+                },
+                icon: const Icon(
+                  Icons.arrow_back_outlined,
+                  color: ManagerColors.white,
+                ),
+              )
+            : Container(),
       ),
-      child: child,
+      extendBodyBehindAppBar: true,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              image,
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: child,
+      ),
     ),
   );
 }
