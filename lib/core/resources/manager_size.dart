8972cc7181b16b109946eaea42936b7240bf13d9
hosamshaban <97hosams@gmail.com>
2023-06-04 16:48:19 +0300

    add sizes

--- lib/core/resources/manager_size.dart
+++ lib/core/resources/manager_size.dart
@@ -8,14 +8,18 @@ class ManagerHeight {
   static double h10 = SizeUtil.setHeight(10);
   static double h12 = SizeUtil.setHeight(12);
   static double h16 = SizeUtil.setHeight(16);
+  static double h18 = SizeUtil.setHeight(18);
   static double h20 = SizeUtil.setHeight(20);
   static double h30 = SizeUtil.setHeight(30);
   static double h40 = SizeUtil.setHeight(40);
+  static double h48 = SizeUtil.setHeight(48);
   static double h50 = SizeUtil.setHeight(52);
   static double h60 = SizeUtil.setHeight(60);
   static double h80 = SizeUtil.setHeight(80);
   static double h70 = SizeUtil.setHeight(70);
   static double h86 = SizeUtil.setHeight(86);
+  static double h90 = SizeUtil.setHeight(90);
+  static double h100 = SizeUtil.setHeight(100);
   static double h206 = SizeUtil.setHeight(206);
 }
 
@@ -27,10 +31,12 @@ class ManagerWidth {
   static double w10 = SizeUtil.setWidth(10);
   static double w12 = SizeUtil.setWidth(12);
   static double w16 = SizeUtil.setWidth(16);
+  static double w18 = SizeUtil.setWidth(18);
   static double w20 = SizeUtil.setWidth(20);
   static double w32 = SizeUtil.setWidth(32);
   static double w50 = SizeUtil.setWidth(50);
   static double w65 = SizeUtil.setWidth(65);
+  static double w100 = SizeUtil.setWidth(100);
   static double w166 = SizeUtil.setWidth(166);
 }
 
@@ -41,6 +47,7 @@ class ManagerRadius {
   static double r8 = SizeUtil.setRadius(8);
   static double r10 = SizeUtil.setRadius(10);
   static double r12 = SizeUtil.setRadius(12);
+  static double r14 = SizeUtil.setRadius(14);
   static double r44 = SizeUtil.setRadius(44);
 }
 
