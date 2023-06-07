dd48e2703914ef504826bd8defedae2eae5c49d7
hosamshaban <97hosams@gmail.com>
2023-06-07 12:34:03 +0300

    create register controller

--- lib/features/auth/presentation/controller/registert_controller.dart
+++ lib/features/auth/presentation/controller/registert_controller.dart
@@ -1,7 +1,11 @@
 import 'package:acthub/config/dependency_injection.dart';
-import 'package:acthub/core/storage/local/app_settings_shared_preferences.dart';
+import 'package:acthub/core/resources/manager_size.dart';
+import 'package:acthub/core/resources/manager_string.dart';
+import 'package:acthub/core/state_renderer/state_renderer.dart';
+import 'package:acthub/core/widgets/dialog_button.dart';
+import 'package:acthub/features/auth/domain/use_case/register_use_case.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:get/get.dart';
 
 class RegisterController extends GetxController {
   late TextEditingController email = TextEditingController();
@@ -10,8 +14,87 @@ class RegisterController extends GetxController {
   late TextEditingController password = TextEditingController();
   late TextEditingController confirmPassword = TextEditingController();
   var formKey = GlobalKey<FormState>();
-  final AppSettingsSharedPreferences _appSettingsSharedPreferences =
-      instance<AppSettingsSharedPreferences>();
+  final RegisterUseCase _registerUseCase = instance<RegisterUseCase>();
+  bool isAgreementPolicy = false;
 
-  Future<void> register(BuildContext context) async {}
+  changePolicyStatus(bool status) {
+    isAgreementPolicy = status;
+    update();
+  }
+
+  void performRegister(BuildContext context) {
+    if (formKey.currentState!.validate()) {
+      if (!isAgreementPolicy) {
+        register(context);
+      } else {
+        dialogRender(
+            context: context,
+            stateRenderType: StateRenderType.popUpErrorState,
+            message: ManagerString.shouldAgreePolicy,
+            title: ManagerString.error,
+            child: dialogButton(
+                message: ManagerString.ok,
+                onPressed: () {
+                  Get.back();
+                }));
+      }
+    }
+  }
+
+  Future<void> register(BuildContext context) async {
+    dialogRender(
+      context: context,
+      stateRenderType: StateRenderType.popUpLoadingState,
+      message: ManagerString.loading,
+      title: '',
+    );
+    (await _registerUseCase.execute(
+      RegisterUseCaseInput(
+        name: fullName.text,
+        email: email.text,
+        password: password.text,
+        confirmationPassword: confirmPassword.text,
+        phone: phone.text,
+      ),
+    ))
+        .fold((l) {
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpErrorState,
+        message: l.message,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: dialogButton(
+              message: ManagerString.ok,
+              onPressed: () {
+                Get.back();
+              }),
+        ),
+      );
+    }, (r) {
+      Get.back();
+      dialogRender(
+        context: context,
+        stateRenderType: StateRenderType.popUpSuccessState,
+        message: ManagerString.thanks,
+        title: '',
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: ManagerWidth.w65,
+          ),
+          child: dialogButton(
+            onPressed: () {
+              Get.back();
+              // Get.offAllNamed(Routes.homeView);
+            },
+            message: ManagerString.thanks,
+          ),
+        ),
+      );
+    });
+  }
 }
