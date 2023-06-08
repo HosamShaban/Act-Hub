3d4424471e113d2885ae3b87528de0d522b0e74d
hosamshaban <97hosams@gmail.com>
2023-06-08 23:11:23 +0300

    create VerificationUseCase

---
+++ lib/features/verification/domain/usecase/verification_usecase.dart
@@ -0,0 +1,31 @@
+import 'package:acthub/core/error_handler/error_handler.dart';
+import 'package:acthub/core/use_case/base_use_case.dart';
+import 'package:acthub/features/verification/data/request/verfification_request.dart';
+import 'package:acthub/features/verification/domain/model/verification_model.dart';
+import 'package:acthub/features/verification/domain/repository/verification_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class VerificationUseCase
+    extends BaseUseCase<VerificationUseCaseInput, VerificationModel> {
+  final VerificationRepository _verificationRepository;
+
+  VerificationUseCase(this._verificationRepository);
+
+  @override
+  Future<Either<Failure, VerificationModel>> execute(
+      VerificationUseCaseInput input) async {
+    return await _verificationRepository.verifyEmail(
+      VerificationRequest(
+        email: input.email,
+        otp: input.otp,
+      ),
+    );
+  }
+}
+
+class VerificationUseCaseInput {
+  String email;
+  String otp;
+
+  VerificationUseCaseInput({required this.email, required this.otp});
+}
