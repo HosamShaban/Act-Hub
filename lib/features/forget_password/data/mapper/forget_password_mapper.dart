7d8cb815f719b94a833a176274d0dad85dbbc630
hosamshaban <97hosams@gmail.com>
2023-06-11 19:58:05 +0300

    create ForgetPasswordMapper

---
+++ lib/features/forget_password/data/mapper/forget_password_mapper.dart
@@ -0,0 +1,10 @@
+import 'package:acthub/core/extensions/extensions.dart';
+
+import '../../domain/models/forget_password.dart';
+import '../response/forget_password_response.dart';
+
+extension ForgetPasswordMapper on ForgetPasswordResponse {
+  toDomain() => ForgetPassword(
+        status: status!.onNull(),
+      );
+}
