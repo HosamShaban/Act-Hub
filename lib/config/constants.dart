6fadd43f449f9c2426532efb0dc9f4c486534b65
hosamshaban <97hosams@gmail.com>
2023-06-11 19:28:03 +0300

    add consts

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -12,6 +12,7 @@ class Constants {
   static const int loginTimer = 1;
   static const double arrowBackButtonElevation = 0;
   static const int codeVerificationMaxLine = 1;
+  static const double elevation = 0;
 }
 
 class ConstantsPrefsKey {
