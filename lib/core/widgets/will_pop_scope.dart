1c00452992606b5200647520229f1a982c27a75e
hosamshaban <97hosams@gmail.com>
2023-06-15 18:41:47 +0300

    edit

--- lib/core/widgets/will_pop_scope.dart
+++ lib/core/widgets/will_pop_scope.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 
 Widget willPopScope({
   required Widget child,
-}){
+}) {
   return WillPopScope(
     child: child,
-      onWillPop: () async => false,
+    onWillPop: () async => false,
   );
 }
