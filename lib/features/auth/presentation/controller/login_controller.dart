8fac3ce7f8fcb9df0109f7d12dcba3a561e98f71
hosamshaban <97hosams@gmail.com>
2023-06-20 19:28:08 +0300

    send fcm token

--- lib/features/auth/presentation/controller/login_controller.dart
+++ lib/features/auth/presentation/controller/login_controller.dart
@@ -1,17 +1,15 @@
 import 'package:acthub/config/constants.dart';
 import 'package:acthub/config/dependency_injection.dart';
 import 'package:acthub/core/extensions/extensions.dart';
-import 'package:acthub/core/resources/manager_colors.dart';
-import 'package:acthub/core/resources/manager_fonts.dart';
 import 'package:acthub/core/resources/manager_size.dart';
 import 'package:acthub/core/resources/manager_string.dart';
-import 'package:acthub/core/resources/manager_styles.dart';
 import 'package:acthub/core/state_renderer/state_renderer.dart';
 import 'package:acthub/core/storage/local/app_settings_shared_preferences.dart';
 import 'package:acthub/core/widgets/dialog_button.dart';
-import 'package:acthub/core/widgets/mainButton.dart';
+import 'package:acthub/features/auth/domain/use_case/fcm_token_use_case.dart';
 import 'package:acthub/features/auth/domain/use_case/login_use_case.dart';
 import 'package:acthub/routes/routes.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
@@ -23,6 +21,7 @@ class LoginController extends GetxController {
   final AppSettingsSharedPreferences _appSettingsSharedPreferences =
       instance<AppSettingsSharedPreferences>();
   bool rememberMe = false;
+  final FcmTokenUseCase _tokenUseCase = instance<FcmTokenUseCase>();
 
   changeRememberMe(bool status) {
     rememberMe = status;
@@ -63,7 +62,10 @@ class LoginController extends GetxController {
               }),
         ),
       );
-    }, (r) {
+    }, (r) async {
+      String? fcmToken = await FirebaseMessaging.instance.getToken();
+      _tokenUseCase.execute(FcmTokenUseCaseInput(fcmToken: fcmToken));
+
       if (rememberMe) {
         _appSettingsSharedPreferences.setEmail(email.text);
         _appSettingsSharedPreferences.setPassword(password.text);
@@ -80,28 +82,20 @@ class LoginController extends GetxController {
           padding: EdgeInsets.symmetric(
             horizontal: ManagerWidth.w65,
           ),
-          child: mainButton(
-            child: Text(
-              ManagerString.ok,
-              style: getMediumTextStyle(
-                fontSize: ManagerFontSize.s16,
-                color: ManagerColors.white,
-              ),
-            ),
+          child: dialogButton(
             onPressed: () {
               Get.back();
+              Get.offAllNamed(Routes.mainView);
             },
-            color: ManagerColors.primaryColor,
-            height: ManagerHeight.h40,
+            message: ManagerString.thanks,
           ),
         ),
-        retryAction: () {},
       );
       Future.delayed(
           const Duration(
             seconds: Constants.loginTimer,
           ), () {
-        Get.offAllNamed(Routes.homeView);
+        Get.offAllNamed(Routes.mainView);
       });
     });
   }
