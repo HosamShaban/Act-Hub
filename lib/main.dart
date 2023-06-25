80c1ab8dc4e41f06e3037ff3b2c7004626b14618
hosamshaban <97hosams@gmail.com>
2023-06-25 14:41:04 +0300

    add EasyLocalization config to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:acthub/config/constants.dart';
 import 'package:acthub/config/dependency_injection.dart';
+import 'package:acthub/config/localization/localization_settings.dart';
+import 'package:acthub/core/resources/manager_assets.dart';
 import 'package:acthub/core/service/theme_service.dart';
 import 'package:acthub/routes/routes.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 void main() async {
   await initModule();
-  runApp(MyApp());
+  runApp(
+    EasyLocalization(
+      supportedLocales: localeSettings.supportedLocales,
+      path: translationPath,
+      fallbackLocale: localeSettings.defaultLocale,
+      startLocale: localeSettings.defaultLocale,
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -27,6 +38,9 @@ class MyApp extends StatelessWidget {
       ),
       builder: (context, child) {
         return GetMaterialApp(
+          localizationsDelegates: context.localizationDelegates,
+          locale: context.locale,
+          supportedLocales: context.supportedLocales,
           debugShowCheckedModeBanner: false,
           onGenerateRoute: RouteGenerator.getRoute,
           initialRoute: Routes.splashView,
