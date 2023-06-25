3ffc64addbf705a1ff0ef1ff289ed3c4ece0cf15
hosamshaban <97hosams@gmail.com>
2023-06-25 14:28:43 +0300

    add consts

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -19,6 +19,11 @@ class Constants {
   static const String notificationChannelDescription =
       'notificationChannelDescription';
   static const String notificationIconName = 'notificationIconName';
+  static const String bearer = 'Bearer';
+  static const String arabicKey = 'ar';
+  static const String englishKey = 'en';
+  static const String arabic = 'العربية';
+  static const String english = 'English';
 }
 
 class ConstantsPrefsKey {
@@ -27,6 +32,7 @@ class ConstantsPrefsKey {
   static const String email = 'email';
   static const String password = 'password';
   static const String loggedIn = 'logged_in';
+  static const String locale = 'locale';
 }
 
 class ApiConstants {
@@ -55,4 +61,9 @@ class ApiConstants {
   static const String contentType = 'Content-Type';
   static const String applicationJson = 'application/json';
   static const String fcmToken = 'fcm_token';
+  static const String avatar = 'avatar';
+  static const String type = 'type';
+  static const String id = 'id';
+  static const String attributes = 'attributes';
+  static const String data = "data";
 }
