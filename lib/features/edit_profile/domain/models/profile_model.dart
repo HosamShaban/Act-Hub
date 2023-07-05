0b0449a5d389a9795e0128df06740232b5ecb20f
hosamshaban <97hosams@gmail.com>
2023-07-06 01:50:54 +0300

    create ProfileModel

---
+++ lib/features/edit_profile/domain/models/profile_model.dart
@@ -0,0 +1,21 @@
+import 'package:acthub/features/edit_profile/domain/models/profile_attributes_model.dart';
+
+import '../../../../core/base_model/base_response.dart';
+
+class ProfileModel extends BaseModel {
+  ProfileModelData data;
+
+  ProfileModel({required this.data, required super.message});
+}
+
+class ProfileModelData {
+  String type;
+  int id;
+  ProfileAttributesModel attributes;
+
+  ProfileModelData({
+    required this.type,
+    required this.id,
+    required this.attributes,
+  });
+}
